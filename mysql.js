4bf6c4d9407b3a6abf29e2bee1731a5410b71a4f
Jankeeeeee <71440034+Jankeeeeee@users.noreply.github.com>
2022-07-20 23:42:36 +0800

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -3,7 +3,9 @@ var pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     password: 'abc123',
-    database: 'crawl'
+    database: 'crawl',
+    connecTimeout:500,    //超时
+    connectionLimit:100   //最大连接数
 });
 var query = function(sql, sqlparam, callback) {
     pool.getConnection(function(err, conn) {
@@ -11,15 +13,17 @@ var query = function(sql, sqlparam, callback) {
             callback(err, null, null);
         } else {
             conn.query(sql, sqlparam, function(qerr, vals, fields) {
-                conn.release(); //释放连接 
+                // conn.release(); //释放连接 
                 callback(qerr, vals, fields); //事件驱动回调 
             });
         }
+        pool.releaseConnection(conn); //
     });
 };
 var query_noparam = function(sql, callback) {
     pool.getConnection(function(err, conn) {
         if (err) {
+            conn.release(); //释放连接
             callback(err, null, null);
         } else {
             conn.query(sql, function(qerr, vals, fields) {
